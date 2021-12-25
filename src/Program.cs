ce5268282a67f8b108ed21a0035ddb0db52ff6d6
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-12-25 02:00:42 +0100

    feat(App/Version-check): Implementation of a version check on opening

--- src/Program.cs
+++ src/Program.cs
@@ -7,11 +7,16 @@ using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Linq;
 using System.Linq;
 using System.IO;
+using System.Net;
+using System.IO.Compression;
 
 namespace LinearsBot
 {
     public class Program
     {
+		private readonly string version = "1.0.0";
+		private WebClient webClient;
+
 		static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
 		public async Task MainAsync()
@@ -25,6 +30,15 @@ namespace LinearsBot
 			if (!File.Exists("data/serverslist"))
 				File.Create("data/serverslist");
 
+			JObject config = Functions.GetConfig();
+			if (config["keepUpdated"].Value<bool>())
+			{
+				webClient = new WebClient();
+				Uri webVersion = new Uri("https://github.com/Cu-chi/linears-utils/blob/master/version");
+				webClient.DownloadStringAsync(webVersion);
+				webClient.DownloadStringCompleted += DownloadStringVersionCompleted;
+			}
+
 			using var services = ConfigureServices();
 
 			Functions.ColoredMessage(ConsoleColor.Black, ConsoleColor.Yellow, "-> Le bot démarre...");
@@ -33,13 +47,42 @@ namespace LinearsBot
 			client.Log += Log;
 			services.GetRequiredService<CommandService>().Log += Log;
 
-			JObject config = Functions.GetConfig();
 			string token = config["token"].Value<string>();
 			await client.LoginAsync(TokenType.Bot, token);
 			await client.StartAsync();
 
 			await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
 			await Task.Delay(-1);
+		}
+
+		private void DownloadStringVersionCompleted(object sender, DownloadStringCompletedEventArgs e)
+		{
+			string latestVersion = e.Result;
+
+			if (latestVersion != version)
+			{
+				Functions.ColoredMessage(ConsoleColor.Black, ConsoleColor.Red, "-> Une nouvelle version est disponible :");
+				Functions.ColoredMessage(ConsoleColor.Black, ConsoleColor.Yellow, "-> Version locale : " + version);
+				Functions.ColoredMessage(ConsoleColor.Black, ConsoleColor.Green, "-> Version distante : " + latestVersion);
+				Console.WriteLine();
+				Functions.ColoredMessage(ConsoleColor.Black, ConsoleColor.Magenta, "-> La version va être télécharhée vers \"\"");
+
+				Uri fileLink = new Uri("https://github.com/Cu-chi/CClick/releases/download/{latestVersion}/CClick-{latestVersion}.zip");
+				webClient.DownloadFileAsync(fileLink, "new-linears-bot.zip");
+				webClient.DownloadFileCompleted += WebClient_DownloadFileCompleted;
+			}
+		}
+
+		private void WebClient_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+		{
+			if (e.Cancelled || e.Error != null)
+			{
+				Functions.ColoredMessage(ConsoleColor.Black, ConsoleColor.Red, "-> Le téléchargement n'a pas pu se finir, essayez de l'effectuer manuellement en se rendant sur le lien du projet github :\n-> https://github.com/Cu-chi/linears-utils/releases");
+				return;
+			}
+			Directory.CreateDirectory("lastupdate");
+			ZipFile.ExtractToDirectory("new-linears-bot.zip", "lastupdate");
+			Functions.ColoredMessage(ConsoleColor.Black, ConsoleColor.Green, "-> Nouvelle version téléchargée avec succès vers :\n-> " + Directory.GetCurrentDirectory() + "\\lastupdate");
 		}
 
 		public ServiceProvider ConfigureServices()
