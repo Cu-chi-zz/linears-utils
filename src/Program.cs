a7a57294d1ad08c0124ed48fa93edd26d5560913
Cuchi <42467470+Cu-chi@users.noreply.github.com>
2021-12-26 02:30:16 +0100

    tweak(version)

--- src/Program.cs
+++ src/Program.cs
@@ -15,7 +15,7 @@ namespace LinearsBot
 {
 	public class Program
 	{
-		private readonly ushort[] version = new ushort[3] { 1, 2, 11 }; // Major, Minor, Patch
+		private readonly ushort[] version = new ushort[3] { 1, 3, 11 }; // Major, Minor, Patch
 		private WebClient webClient;
 
 		static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
